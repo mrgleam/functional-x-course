c1feb00baa855e5cc976281715aaee681639b753
Santi Lertsumran <santi@odds.team>
2025-03-06 17:24:41 +0700

    feat: add euclid algorithm for solve gcd

--- examples/haskell/gcd/app/Main.hs
+++ examples/haskell/gcd/app/Main.hs
@@ -5,3 +5,5 @@ import Lib
 main :: IO ()
 main = do
   print $ gcd' 48 18
+  print $ euclid 48 18
+  print $ euclid' 48 18
--- examples/haskell/gcd/src/Lib.hs
+++ examples/haskell/gcd/src/Lib.hs
@@ -1,5 +1,7 @@
 module Lib
-    ( gcd'
+    ( gcd',
+      euclid,
+      euclid'
     ) where
 
 import Numeric.Natural (Natural)
@@ -7,3 +9,10 @@ import Numeric.Natural (Natural)
 gcd' :: Natural -> Natural -> Natural
 gcd' a b = maximum [d | d <- [1..min a b], a `mod` d == 0, b `mod` d == 0]
 
+euclid :: Natural -> Natural -> Natural
+euclid a b = if b == 0 then a else euclid b (a `mod` b)
+
+euclid' :: Natural -> Natural -> Natural
+euclid' a b 
+  | b == 0    = a
+  | otherwise = euclid' b (a `mod` b)
